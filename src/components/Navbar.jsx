13771d25666e31b0f510be13ea366e0a03b59a33
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-07-30 18:10:30 +0530

    updated the cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,14 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-800 shadow-md sticky top-0 z-50 w-full">
       <div className="w-[90%] mx-auto flex items-center justify-between h-14">
-        {/* Left: Logo */}
+        
         <Link to="/" className="text-lg font-bold text-white">
           ShopCart
         </Link>
 
-        {/* Right: Cart + Hamburger */}
+        
         <div className="flex items-center gap-5">
-          {/* Cart */}
+          
           <Link
             to="/cart"
             className="relative flex items-center justify-center text-white hover:text-blue-300 w-6 h-6"
@@ -31,7 +31,7 @@ const Navbar = () => {
             )}
           </Link>
 
-          {/* Hamburger */}
+          
           <div
             className="md:hidden flex flex-col justify-center items-center w-6 h-6 cursor-pointer z-50"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -55,7 +55,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Slide-in Sidebar Drawer */}
+      
       <div
         className={`fixed top-0 right-0 h-full w-64 bg-blue-900 text-white shadow-lg transform transition-transform duration-300 ease-in-out z-40 ${
           menuOpen ? "translate-x-0" : "translate-x-full"
@@ -88,7 +88,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Overlay */}
+
       {menuOpen && (
         <div
           className="fixed inset-0 bg-black opacity-40 z-30 md:hidden"
